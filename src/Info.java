f071bd27ce099a3cd6295f64339afdf9efdf9fd9
vrezhhakobyan <vrej.hakobyan2003@gmail.com>
2024-04-14 11:34:52 +0400

    not final version

--- src/Info.java
+++ src/Info.java
@@ -1,5 +1,5 @@
 public class Info {
 	public static void print(Player player, String msg) {
-		System.out.println("[Turn " + (player.getTotalWalk() + 1) + "] [" + player.getCurrentPosition() + "] [$" + player.getMoney().getMoney() + "] " + msg);
+		System.out.println((player.getTotalWalk() + 1) +  player.getCurrentPosition() +  player.getMoney().getMoney() +  msg);
 	}
 }
